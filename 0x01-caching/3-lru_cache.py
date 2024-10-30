2be1e5f5e9e1d40b9d94866f3837bbff1769d1fe
SeM2x <houssem135.302@gmail.com>
2024-10-31 00:25:11 +0100

    3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" module to implement LRUCache class """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU cache class """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        from collections import OrderedDict
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is None or item is None:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
